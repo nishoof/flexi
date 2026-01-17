8c75c33fe5b2b2f7d284d53ba78e08490b04aa2d
nishoof <nishilanand21@gmail.com>
2026-01-16 19:23:01 -0800

    feat: add border to stat card

--- frontend/src/components/StatCard.tsx
+++ frontend/src/components/StatCard.tsx
@@ -1,10 +1,8 @@
-function StatCard({ title, value }: { title: string; value: number }) {
+export default function StatCard({ title, value }: Readonly<{ title: string; value: number }>) {
   return (
-    <div className="grow aspect-2/1 bg-(--background-light) p-4 rounded-lg">
+    <div className="grow aspect-2/1 bg-(--background-light) p-4 rounded-lg border border-(--border)">
       <h2 className="text-lg font-semibold mb-2">{title}</h2>
       <p className="text-3xl font-bold">${value}</p>
     </div>
   );
 }
-
-export default StatCard;
